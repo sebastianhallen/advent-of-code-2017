808d6e83f011b37f74ea20ac6244bd75de2c0f26
Sebastian Hallén <sebastian.hallen@tretton37.com>
2017-12-05 07:53:54 +0100

    day 5

---
+++ day5.1.js
@@ -0,0 +1,43 @@
+const { test } = require('./fluff');
+const fs = require('fs');
+
+const sample = {
+  input: [0, 3, 0, 1, -3],
+  answer: 1,
+};
+
+function print(instructions, pointer) {
+  console.log(instructions.map((instruction, i) => i === pointer ? `(${instruction})` : instruction));
+}
+
+function modifyInstruction(instruction) {
+  return instruction + 1;
+}
+
+function solve(instructions) {
+  let pointer = 0;
+  for(let i = 1; true; i++) {
+    //print(instructions, pointer);
+    console.log(pointer);
+    instruction = instructions[pointer];
+    instructions[pointer] = modifyInstruction(instructions[pointer]);
+    
+    pointer += instruction;
+
+    if (pointer >= instructions.length) {
+      //print(instructions);
+      return i;
+    }
+  }
+}
+
+console.log(solve(sample.input))
+
+fs.readFile('day5.input', (err, data) => {
+  if (err) {
+    console.error(err);
+    return;
+  }
+
+  console.log(solve(data.toString().split('\n').map(i => parseInt(i, 10))));
+});
