da7d2a6e08e41765087285bda9a66c0837238825
Sebastian Hallén <sebastian.hallen@tretton37.com>
2017-12-15 19:32:15 +0100

    fix day7.2

--- day7.2.js
+++ day7.2.js
@@ -36,139 +36,50 @@ function asTree(data) {
   function appendChildren(rootNode, level = 0) {
     rootNode.level = level;
     rootNode.children = rootNode.children.map(child => appendChildren(nodes.filter(node => node.name === child)[0], level + 1));
-    //rootNode.childWeights = rootNode.children.map(child => child.weight);
-    //rootNode.childWeightsDebug = JSON.stringify(rootNode.childWeights);
+    rootNode.levelWeight = rootNode.weight + rootNode.children.reduce((w, child) => w + child.levelWeight, 0);
     return rootNode;
   }
 
-  
   return appendChildren(root)
 }
 
-function traverse(root, visitNode, reverse = false) {
-  if (reverse) {
-    root.children.forEach(child => traverse(child, visitNode, reverse));
-    backpropagate(root, visitNode);
-  }
-  else {
-    visitNode(root);
-    root.children.forEach(child => traverse(child, visitNode, reverse));
-  }
-}
-
-function backpropagate(child, visitNode) {
-  //console.log(`backpropagating from ${child.name}: ${child.parent ? child.parent.name : '()'}`);
-  if (child.parent) {
-    visitNode(child);
-    backpropagate(child.parent, visitNode);
-  }
-}
-
-function isUnbalanced(node) {
-  //const childWeights = [...new Set(node.children.map(item => item.weigh))];
-  const childWeights = node.children.reduce((freq, child) => {
-    const id = `${child.levelWeight}`;
-    if (freq[id]) freq[id] += 1;
-    else freq[id] = 1;
-    return freq;
-  }, {});
-
-  console.log(`${node.name}: ${JSON.stringify(childWeights)}`);
-  if (Object.keys(childWeights).length > 1) {
-    console.log('you done goofed');
-    //console.log(node);
-    
-  }
-  return Object.keys(childWeights).length > 1;
-/*  return node.children.reduce((freq, child) => {
-    const id = `${child.name}`;
-    if (freq[id]) freq[id] += 1;
-    else freq[id] = 1;
-  }, {})*/
+function traverse(root, visitNode) {
+  visitNode(root);
+  root.children.forEach(child => traverse(child, visitNode));
 }
 
 function isBalanced(node) {
-  const childWeights = node.children.reduce((freq, child) => {
-    const id = `${child.weight}`;
-    if (freq[id]) freq[id] += 1;
-    else freq[id] = 1;
-    return freq;
-  }, {});
-
-  if (Object.keys(childWeights).length == 2) {
-    console.log(node);
-    console.log(childWeights);
+  const childW = childWeights(node);
+  if (childW.length > 1) {
+    return false;
   }
-  return Object.keys(childWeights).length <= 1;
+
+  return true;
+}
+
+function childWeights(node) {
+  const childWeights = [...new Set(node.children.map(c => c.levelWeight))];
+  return childWeights;
 }
 
 function solve(data) {  
   const tree = asTree(data);
+  let minLevelWeight = tree.levelWeight;
+  let minLevelWeightParent = tree;
 
   traverse(tree, node => {
-    if (isBalanced(node)) {
-      // console.log('node is balanced')
-      //console.log(node);
-      //console.log(node.parent);
-
-      if (node.children.length > 0 && node.parent.children.filter(x => !isBalanced(x)).length === 0) {
-        console.log('candidate')
-        console.log(node.parent)
-        //process.exit(0)
-        //process.exit(0);
+    if (isBalanced(node) && !isBalanced(node.parent) && minLevelWeight > node.parent.levelWeight) {
+      if (minLevelWeight > node.parent.levelWeight) {
+        minLevelWeight = node.parent.levelWeight;
+        minLevelWeightParent = node.parent;
       }
-      
     }
   });
-return;
 
-  traverse(tree, child => {
-    if (child.children.length === 0) {
-      child.levelWeight = child.weight;
-    }
-    const parent = child.parent;
-  
-    //console.log(child);
-    if (parent) {
-      if (parent.levelWeight) {
-        //console.log(`updating parents levelWeight from ${parent.levelWeight} to ${parent.levelWeight + child.weight} (${parent.levelWeight} + ${child.weight})`);
-        parent.levelWeight = parent.levelWeight + child.weight;  
-      }
-      else {
-        //console.log(`setting parents levelWeight to ${parent.weight + child.weight} (${parent.weight} + ${child.weight})`);
-        parent.levelWeight = parent.weight + child.weight;
-      }
-    }
-    //console.log('foo')
-    //console.log([...new Set(child.children.map(item => item.weight))])
-  }, false);
-
-  let n = tree;
-  function foo() {
-    while(isUnbalanced(n)) {
-      for (let i = 0; i < n.children.length; i++) {
-        n = n.children[i];
-      }
-    }
-  } 
-  foo();
-  console.log(n);
-  /*
-  traverse(tree, node => {
-    if (isUnbalanced(node))  {
-      console.log('unbalanced node!')
-      //console.log(childLevelWeights);
-      console.log(node);
-      //console.log(`diff: ${childLevelWeights[0]} - ${childLevelWeights[1]} === ${childLevelWeights[0] - childLevelWeights[1]}`)
-      //console.log(node.parent);
-      //process.exit(0);
-    }
-  }, true);
-  */
- // console.log(tree);
+  console.log(`one of the following nodes has the wrong weight: 
+${minLevelWeightParent.children.map(c => `${c.name} ${c.levelWeight} : ${c.weight}`).join('\n')}  
+`);
 }
-
-//console.log(solve(sample));
 
 fs.readFile('day7.input', (err, data) => {
   if (err) {
